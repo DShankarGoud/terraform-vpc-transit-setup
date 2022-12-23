9b0198af24033e502ea3cb367c471a3d4cf57c95
shiva shankar <sivas1475@gmail.com>
2022-12-24 00:58:28 +0530

    JENKINS

--- data.tf
+++ data.tf
@@ -1,7 +1,3 @@
 data "aws_subnet_ids" "default-vpc" {
   vpc_id = var.DEFAULT_VPC_ID
 }
-
-data "aws_vpc" "default" {
-  id = var.DEFAULT_VPC_ID
-}
--- env-dev/dev.tfvars
+++ env-dev/dev.tfvars
@@ -1,2 +1,3 @@
 DEFAULT_VPC_ID = "vpc-00e7859e09c0992d4"
 ENV            = "dev"
+DEFAULT_VPC_ID = "rtb-0e2f697b49a492acc"
