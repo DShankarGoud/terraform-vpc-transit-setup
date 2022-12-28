66bcacfeb3e44d07ac6c0f586c2add595ea13b16
shiva shankar <sivas1475@gmail.com>
2022-12-29 05:10:13 +0530

    JENKINS

--- data.tf
+++ data.tf
@@ -1,10 +1,10 @@
-data "aws_subnet_ids" "default-vpc" {
-  vpc_id = var.DEFAULT_VPC_ID
-}
-
 data "aws_subnets" "ngw"{
   filter {
     name   = "tag:NGW"
     values = ["true"]
   }
+}
+
+data "aws_vpc" "default" {
+  vpc_id = var.DEFAULT_VPC_ID
 }
